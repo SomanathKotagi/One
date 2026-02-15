94bfff59a30b86ef48f884d0f9bc8e36bd5a5309
SomanathKotagi <pratikkotagi2003@gmail.com>
2026-02-15 14:54:56 +0530

    Added converting tuple to list

--- map_function.py
+++ map_function.py
@@ -6,3 +6,14 @@ print(list(map(myfunc,('apple','mango'))))
 def square(r):
     return 3.14*r*r
 print(square(6))
+
+def liststring(name):
+    return name;
+
+print(list(map(liststring,("Somanath","Anish","Prajwal","Prasad","Vijay","Pravin","Karthik","Mahesh"))))
+
+def morethantwo(names):
+    return names
+
+print(tuple(map(morethantwo,[['Somanath','Kotagi','Suraj','Patil','Balkrishna','Arjunwadkar','Priiya Raut'],['Darshan Raut','Nilesh Somanache','Shivam ','Suryakant']])))
+
